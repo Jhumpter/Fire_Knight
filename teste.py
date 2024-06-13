03c23698d46e794eef64179cd6ffe9ee0f878580
Jhumpter <joaopwii@gmail.com>
2024-06-13 12:51:39 -0300

    Bola

--- teste.py
+++ teste.py
@@ -1,31 +1,49 @@
-import time
 import pygame
-# definindo cores em RGB
+# definindo cores
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-pygame.init()  # Inicializando módulos
-screen = pygame.display.set_mode([640, 480])  # Criando superfície para janela (screen) do tamanho indicado
-font = pygame.font.SysFont('None', 55)  # Carregando fonte
-pygame.display.set_caption('Olá mundo')  # Nomeando a janela
-screen.fill(BLACK)  # Preenchendo a janela com uma cor (preto)
-# Desenhando na superfície. No pygame, para o plano de desenho, o y cresce pra baixo e o x pra direita
-# draw.line(superfície, cor, [coordenadas início], [coordenadas fim], largura(pixels))
-# draw.rect(superfície, cor, [x(início), y(início), largura, altura])
-# draw.ellipse(superfície, cor, [x(início), y(início), largura, altura])
-# draw.polygon(superfície, cor, [[cordenada1], [coordenada2], [coordenadaN]...])
-pygame.draw.line(screen, WHITE, [10, 100], [630, 100], 5)
-pygame.draw.rect(screen, BLUE, [200, 210, 40, 20])
-pygame.draw.ellipse(screen, RED, [300, 200, 40, 40])
-pygame.draw.polygon(screen, GREEN, [[420, 200], [440, 240], [400, 240]])
-pygame.display.flip()  # Atualiza a janela com o conteúdo da superfície
-time.sleep(5)  # Faz o programa esperar 5s pra fechar
-screen.fill(BLACK)
-# Definindo o texto com font.render(texto, antialias(T/F), cor)
-# Antialias suaviza o contorno do texto
-text = font.render('pygame', True, WHITE)
-screen.blit(text, [250, 200])
-pygame.display.flip()
-time.sleep(5)
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Game Loop')
+# variáveis da bola
+position_x = 300
+position_y = 200
+velocity_x = 1
+velocity_y = 1
+# iniciando o loop de jogo
+while True:
+    # PROCESSAMENTO DE ENTRADA
+    # capturando eventos
+    event = pygame.event.poll()
+    # caso o evento QUIT (clicar no x da janela) seja disparado
+    if event.type == pygame.QUIT:
+        # saia do loop finalizando o programa
+        break
+    # ATUALIZAÇÃO DO JOGO
+    # movendo a bola
+    # A cada repetição a posição da bola aumenta em sua velocidade
+    position_x += velocity_x
+    position_y += velocity_y
+    # mudando a direção no eixo x nas bordas
+    # Caso a bola começe a chegar nas bordas da direita ou de baixo, a velocidade será negativa
+    # Caso chegue nas outras bordas, se tornará positiva
+    if position_x > 600:
+        velocity_x = -1
+    elif position_x < 0:
+        velocity_x = 1
+    # mudando a direção no eixo y nas bordas
+    if position_y > 440:
+        velocity_y = -1
+    elif position_y < 0:
+        velocity_y = 1
+    # DESENHO
+    # preenchendo o fundo com preto
+    # É importante sempre pintar o fundo ou se não ficará o rastro da bola
+    screen.fill(BLACK)
+    # desenhando a bola
+    pygame.draw.ellipse(screen, RED, [position_x, position_y, 40, 40])
+    # atualizando a tela
+    pygame.display.flip()
